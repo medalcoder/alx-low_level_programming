c8ebab7d765c2f7711fabb44587db213b0f6393b
AbdulHameed Ismaheel <hammedadeosun@gmail.com>
2023-07-15 10:52:22 -0700

    all 0x01 files created

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Function to get the last digit of a number */
+int getLastDigit(int number) {
+    return abs(number) % 10;
+}
+
+int main(void) {
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    int lastDigit = getLastDigit(n);
+
+    printf("The string Last digit of %d is ", n);
+    
+    if (lastDigit > 5)
+        printf("and is greater than 5\n");
+    else if (lastDigit == 0)
+        printf("and is 0\n");
+    else
+        printf("and is less than 6 and not 0\n");
+
+    return (0);
+}
