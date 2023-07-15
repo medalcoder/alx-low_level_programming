e929c166fbb825c9c5e1087028f946c0f67484c8
AbdulHameed Ismaheel <hammedadeosun@gmail.com>
2023-07-15 14:25:50 -0700

    2nd attempt to check answers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,27 +6,22 @@
  * Return: Always 0 (Success)
  */
 
-/* Function to get the last digit of a number */
-int getLastDigit(int number) {
-    return abs(number) % 10;
-}
-
 int main(void) {
     int n;
 
     srand(time(0));
     n = rand() - RAND_MAX / 2;
 
-    int lastDigit = getLastDigit(n);
-
-    printf("The string Last digit of %d is ", n);
+    printf("%d ", n);
     
-    if (lastDigit > 5)
-        printf("and is greater than 5\n");
-    else if (lastDigit == 0)
-        printf("and is 0\n");
+    if (n > 0)
+        printf("is positive");
+    else if (n == 0)
+        printf("is zero");
     else
-        printf("and is less than 6 and not 0\n");
+        printf("is negative");
 
-    return (0);
+    printf("\n");
+
+    return 0;
 }
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,3 +5,28 @@
  *
  * Return: Always 0 (Success)
  */
+
+int main(void) {
+    int num1, num2;
+
+    for (num1 = 0; num1 <= 98; num1++) {
+        for (num2 = num1 + 1; num2 <= 99; num2++) {
+            putchar('0' + num1 / 10);
+            putchar('0' + num1 % 10);
+            putchar(' ');
+
+            putchar('0' + num2 / 10);
+            putchar('0' + num2 % 10);
+
+            
+            if (num1 != 98 || num2 != 99) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
