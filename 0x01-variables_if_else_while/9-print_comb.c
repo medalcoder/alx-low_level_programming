e224c64002ef8ef33d72407e6a4ab7f2c440b496
AbdulHameed Ismaheel <hammedadeosun@gmail.com>
2023-07-16 08:16:15 -0700

    meas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	char t = 48;
 
-int main(void) {
-    int i;
-
-    // Print numbers 0 to 8 followed by a comma and space using putchar
-    for (i = 0; i < 9; i++) {
-        putchar('0' + i);
-        putchar(','); // Print comma
-        putchar(' '); // Print space
-    }
-
-    // Print the last number 9 followed by a newline using putchar
-    putchar('9');
-    putchar('\n');
+	while (t != 57)
+	{
+		putchar(t);
+		if (t != 57)
+		{
+			putchar(',')
+		}
+		t++;
+	}
+	putchar('\n');
+	putchar();
+	return (0);
+}	
