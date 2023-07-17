16d636179492b140e108d35923f7f3f79878eaf0
AbdulHameed Ismaheel <hammedadeosun@gmail.com>
2023-07-17 07:43:58 -0700

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,15 @@ int main(void)
 {
 int i;
 for (i = 0; i < 9; i++)
-{	
-if (i == 9)
-putchar('0' + i);
-else 
 {
-putchar('0' + 1);
+putchar(i + '0');
+if (i != 9)
+{
 putchar(',');
 putchar(' ');
 }
 
 }
-return(0);
+putchar('\n');
+return (0);
 }
