be7646cae3b94027d4075336bce6b9a1e24963f4
AbdulHameed Ismaheel <hammedadeosun@gmail.com>
2023-07-17 11:09:47 +0000

    mum

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,12 @@ int main(void)
     char i;
 
     for (i = 'a'; i <= 'z'; i++)
-	    putchar(i);
+		putchar(i);
+
     for (i = 'A'; i <= 'Z'; i++)
-	    putchar(i);
+		putchar(i);
+
     putchar('\n');
+
     return (0);
 }
