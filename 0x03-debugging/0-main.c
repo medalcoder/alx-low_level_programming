8cdd6f5fb2761bdb70632c738ec4cacdedf8a145
AbdulHameed Ismaheel <hammedadeosun@gmail.com>
2023-07-18 18:16:13 -0700

    all files created

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ * main - Test for positive or negative function
+ *
+ * Return - always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
+}
