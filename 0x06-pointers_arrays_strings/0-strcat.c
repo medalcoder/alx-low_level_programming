bb2bb9f5c995e15bb765c3e5709719f8f47617dd
AbdulHameed Abidemi <hammedadeosun@gmail.com>
2023-07-26 20:39:06 +0100

    Task One Attempted
    The 0-strcat file is being modified for the function to be tested with
    test file 0-main.c
    _putchar file created to serve as a function to print characters
    The header file main.h is created to contain all the prototype
    for the project

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcat - concatenate 2 strings
+* @dest: destination of concatenation
+* @src: source string
+* Return: dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+    int destlen = 0;
+    int srclen = 0;
+    int h
+
+    for (h = 0 ; dest[h] != '\0' ; h++)
+            destlen++;
+    for (h = 0 ; src[h] != '\0' ; h++)
+            srclen++;
+    for (h = 0 ; h <= srclen ; h++)
+}
+ 
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _strcat - function that concatenates two strings
+* 
+* 
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+}
