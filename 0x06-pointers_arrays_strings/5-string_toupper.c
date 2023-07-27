ea39eda7bcfe549ae3150e79d93278e58ddffc28
AbdulHameed Ismaheel <hammedadeosun@gmail.com>
2023-07-27 07:22:04 -0700

    Task 5 done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @c: string to convert
+ * Return: c
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0 ; c[i] != '\0' ; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 32;
+	}
+	return (c);
+}
