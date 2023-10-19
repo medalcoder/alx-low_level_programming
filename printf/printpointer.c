a7aaaf78251f2a1a9573c2564d04414f404268c1
AbdulHameed <hammedadeosun@gmail.com>
2023-10-19 09:44:05 +0100

    Compilation Error 2 fixed

--- printf/printpointer.c
+++ printf/printpointer.c
@@ -9,12 +9,12 @@
 int print_pointer(va_list val)
 {
     void *p;
-    char *s = "(nil)", long int a;
+    char *s = "(nil)", (long int a);
     int i, b;
 
     p = va_arg(val, void *); if (p == NULL)
     {
-        for (i = 0, s[i] != '\0'; i++)
+        for (i = 0; s[i] != '\0'; i++)
             _putchar(s[i]);
         return (i);
     }
