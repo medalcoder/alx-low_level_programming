94485476a58250515e2a44241a52db9f1ab5ab69
AbdulHameed <hammedadeosun@gmail.com>
2023-10-24 07:22:21 -0700

    Task 1 solved

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - find the length of a list
+ * @h: head node
+ * Return: length of the node
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
